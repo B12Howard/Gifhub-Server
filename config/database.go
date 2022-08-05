1001e0fb81bf23c7cdb2efad20d898e9e1ad91d4
B12Howard <howard.luong87@gmail.com>
2022-08-04 22:34:32 -0700

    refactor(database): use Sprintf to assemble query string

--- config/database.go
+++ config/database.go
@@ -2,8 +2,8 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
-	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -12,9 +12,21 @@ type MyHandler struct {
 	db *sql.DB
 }
 
+const (
+	host     = ""
+	port     = ""
+	user     = ""
+	password = ""
+	dbname   = ""
+)
+
 func NewDb() *sql.DB {
-	connectionString := os.Getenv("CONNECTIONSTRING")
-	db, err := sql.Open("postgres", connectionString)
+	// https://www.calhoun.io/connecting-to-a-postgresql-database-with-gos-database-sql-package/
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
 
 	if err != nil {
 		log.Fatal((err))
