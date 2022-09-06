ed0b9fa284673036414275466f00f176ad0bacd2
B12Howard <howard.luong87@gmail.com>
2022-09-06 02:05:58 -0700

    style(services): remove log.fatal; rename files and vars

--- services/user_files_handler.go
+++ services/user_files_handler.go
@@ -4,23 +4,24 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/render"
 )
 
 type UserFilePagination struct {
-	RowCount int              `json:"rowCount"`
-	LastId   int              `json:"lastId"`
-	LastDate sql.NullTime     `json:"lastDate"`
-	Records  []UserFileRecord `json:"records"`
-	Next     bool             `json:"next"`
+	RowCount int          `json:"rowCount"`
+	LastId   int          `json:"lastId"`
+	LastDate sql.NullTime `json:"lastDate"`
+	Records  []UserFile   `json:"records"`
+	Next     bool         `json:"next"`
 }
 
-type UserFileRecord struct {
-	Id        int          `json:"id"`
-	Url       string       `json:"url"`
-	Createdat sql.NullTime `json:"createdat"`
+type UserFile struct {
+	Id         int          `json:"id"`
+	Url        string       `json:"url"`
+	Created_at sql.NullTime `json:"created_at"`
 }
 
 // GetUserGifs takes in a UserFilePagination and queries the `userfiles` table for gifs converted and saved to the remote storage (GCP Cloud Storage).
@@ -40,33 +41,37 @@ func GetUserGifs(db *sql.DB) func(http.ResponseWriter, *http.Request) {
 		errDecode := decoder.Decode(&data)
 
 		if errDecode != nil {
-			// log.Fatalln(errDecode)
+			log.Println(errDecode)
 			render.JSON(w, r, ("Missing number of pagination rows"))
+
+			return
 		}
 
 		if data.LastDate.Valid == false {
-			rows, errRows = db.Query(`SELECT id, url, createdat FROM userfiles ORDER BY createdat DESC, id DESC FETCH FIRST $1 ROWS ONLY`, data.RowCount)
+			rows, errRows = db.Query(`SELECT id, url, created_at FROM user_files ORDER BY created_at DESC, id DESC FETCH FIRST $1 ROWS ONLY`, data.RowCount)
 
 		} else if data.Next {
-			rows, errRows = db.Query(`SELECT id, url, createdat FROM userfiles WHERE (createdat, id) < ($1, $2) ORDER BY createdat DESC, id DESC FETCH FIRST $3 ROWS ONLY`, data.LastDate.Time, data.LastId, data.RowCount)
+			rows, errRows = db.Query(`SELECT id, url, created_at FROM user_files WHERE (created_at, id) < ($1, $2) ORDER BY created_at DESC, id DESC FETCH FIRST $3 ROWS ONLY`, data.LastDate.Time, data.LastId, data.RowCount)
 
 		} else {
-			rows, errRows = db.Query(`SELECT id, url, createdat FROM userfiles WHERE (createdat, id) > ($1, $2) ORDER BY createdat DESC, id ASC FETCH FIRST $3 ROWS ONLY`, data.LastDate.Time, data.LastId, data.RowCount)
+			rows, errRows = db.Query(`SELECT id, url, created_at FROM user_files WHERE (created_at, id) > ($1, $2) ORDER BY created_at DESC, id ASC FETCH FIRST $3 ROWS ONLY`, data.LastDate.Time, data.LastId, data.RowCount)
 		}
 
 		if errRows != nil {
-			// log.Fatalln(errRows)
+			log.Println(errRows)
 			render.JSON(w, r, ("No records found"))
+
+			return
 		}
 
 		for rows.Next() {
-			userFile := UserFileRecord{}
-			rows.Scan(&userFile.Id, &userFile.Url, &userFile.Createdat)
+			userFile := UserFile{}
+			rows.Scan(&userFile.Id, &userFile.Url, &userFile.Created_at)
 			data.Records = append(data.Records, userFile)
 		}
 		fmt.Print(&data.Records[0])
 		data.LastId = data.Records[len(data.Records)-1].Id
-		data.LastDate = data.Records[len(data.Records)-1].Createdat
+		data.LastDate = data.Records[len(data.Records)-1].Created_at
 
 		render.Status(r, http.StatusOK)
 		render.JSON(w, r, data)
---
+++ services/user_handler.go
@@ -0,0 +1,93 @@
+package services
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/render"
+)
+
+type UserQuery struct {
+	Uid string `json:"uid"`
+}
+
+type UserTypes struct {
+	Name            string
+	File_size_limit int
+	Usage_limit     int
+	Id              int
+	Created_at      sql.NullTime `json:"created_at"`
+	Updated_at      sql.NullTime `json:"updated_at"`
+}
+
+type UserRes struct {
+	id           int
+	uid          string
+	user_type_id int
+	created_at   sql.NullTime
+	ut           UserTypes
+}
+
+func GetUser(db *sql.DB) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var data UserQuery
+		decoder := json.NewDecoder(r.Body)
+		decoder.DisallowUnknownFields()
+
+		errDecode := decoder.Decode(&data)
+
+		if errDecode != nil {
+			log.Println(errDecode)
+			render.JSON(w, r, ("Missing uid"))
+		}
+
+		retrievedUser := &UserRes{}
+		retrievedUserType := &UserTypes{}
+
+		row := db.QueryRow(`SELECT Users.id, Users.created_at, UserTypes.name,  UserTypes.file_size_limit,  UserTypes.created_at, uid FROM users Users INNER JOIN user_types UserTypes ON Users.user_type_id=UserTypes.id WHERE uid = $1`, data.Uid)
+		err := row.Scan(&retrievedUser.id, &retrievedUser.created_at, &retrievedUserType.Name, &retrievedUserType.File_size_limit, &retrievedUserType.Created_at, &retrievedUser.uid)
+
+		if err != nil {
+			log.Println(err)
+			render.Status(r, http.StatusNotFound)
+			render.JSON(w, r, ("No User found with uid " + data.Uid))
+
+			return
+		}
+
+		payload := map[string]interface{}{
+			"id":  &retrievedUser.id,
+			"uid": &retrievedUser.uid,
+			"ut":  &retrievedUserType,
+		}
+
+		render.Status(r, http.StatusOK)
+		render.JSON(w, r, payload)
+	}
+}
+
+func SetUserUsage(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hi"))
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hi"))
+}
+
+// func GetUserUsageById()
+
+// func SetUserUsage(w http.ResponseWriter, r *http.Request) {
+// 	render.Status(r, http.StatusCreated)
+// 	render.JSON(w, r, map[string]string{"stuff": "post"})
+// }
+// func PutHandler(w http.ResponseWriter, r *http.Request) {
+// 	render.Status(r, http.StatusCreated)
+// 	render.JSON(w, r, map[string]string{"stuff": "put"})
+// }
+
+// func DeleteHandler(w http.ResponseWriter, r *http.Request) {
+// 	render.Status(r, http.StatusCreated)
+// 	render.JSON(w, r, map[string]string{"stuff": "delete"})
+// }
